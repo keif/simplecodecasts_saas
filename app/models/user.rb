cda0fdbe69d2e3ab21497d1fcbeaa3273af558b1
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-04-07 01:29:31 -0400

    user_profiles: Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :plan
+  has_one :profile
   attr_accessor :stripe_card_token
+
   def save_with_payment
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
